5c6740b9bb167a9fa535753a9a3da1417c200ad1
Vinicius Silva <vsaguiar@outlook.com>
2024-04-28 18:28:07 -0300

    implementar múltiplas camadas e entidades de domínio

---
+++ Back/src/ProEventos.Domain/Evento.cs
@@ -0,0 +1,16 @@
+namespace ProEventos.Domain;
+
+public class Evento
+{
+    public int Id { get; set; }
+    public string Local { get; set; }
+    public DateTime? DataEvento { get; set; }
+    public string Tema { get; set; }
+    public int QuantidadePessoas { get; set; }
+    public string ImagemURL { get; set; }
+    public string Telefone { get; set; }
+    public string Email { get; set; }
+    public IEnumerable<Lote> Lote { get; set; }
+    public IEnumerable<RedeSocial> RedesSociais { get; set; }
+    public IEnumerable<PalestranteEvento> PalestrantesEventos { get; set; }
+}
