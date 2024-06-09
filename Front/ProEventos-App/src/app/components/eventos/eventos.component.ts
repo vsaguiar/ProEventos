52b638ffabd0b3130853bda628e3b5fcaaeb88f9
Vinicius Silva <vsaguiar@outlook.com>
2024-06-08 23:34:47 -0300

    ajustando tsconfig para paths

--- Front/ProEventos-App/src/app/components/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/components/eventos/eventos.component.ts
@@ -1,11 +1,4 @@
-import { Component, OnInit, TemplateRef } from '@angular/core';
-
-import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
-import { ToastrService } from 'ngx-toastr';
-import { NgxSpinnerService } from 'ngx-spinner';
-
-import { Evento } from '../../models/Evento';
-import { EventoService } from '../../services/evento.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-eventos',
