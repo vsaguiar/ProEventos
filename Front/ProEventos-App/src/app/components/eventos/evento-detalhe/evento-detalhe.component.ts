4967fb1cc4c1a378dc815427b58924aef7c7b145
Vinicius Silva <vsaguiar@outlook.com>
2024-06-13 06:27:27 -0300

    corrigir bug e adicionar cssValidator

--- Front/ProEventos-App/src/app/components/eventos/evento-detalhe/evento-detalhe.component.ts
+++ Front/ProEventos-App/src/app/components/eventos/evento-detalhe/evento-detalhe.component.ts
@@ -43,4 +43,8 @@ export class EventoDetalheComponent implements OnInit {
     this.form.reset();
   }
 
+  public cssValidator(campoForm: FormControl): any {
+    return {'is-invalid': campoForm.errors && campoForm.touched}
+  }
+
 }
