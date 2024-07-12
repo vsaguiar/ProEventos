c7bb3a935303529fba6b12e03a640980cb55306f
Vinicius Silva <vsaguiar@outlook.com>
2024-07-12 15:14:20 -0300

    criar endpoint para upload e métodos salvar e deletar imagem

--- Back/src/ProEventos.API/Controllers/EventosController.cs
+++ Back/src/ProEventos.API/Controllers/EventosController.cs
@@ -9,9 +9,11 @@ namespace ProEventos.API.Controllers;
 public class EventosController : ControllerBase
 {
     private readonly IEventoService _eventoService;
-    public EventosController(IEventoService eventoService)
+    private readonly IWebHostEnvironment _environment;
+    public EventosController(IEventoService eventoService, IWebHostEnvironment environment)
     {
         _eventoService = eventoService;
+        _environment = environment;
     }
 
     [HttpGet]
@@ -65,6 +67,31 @@ public class EventosController : ControllerBase
     }
 
 
+    [HttpPost("upload-image/{eventoId}")]
+    public async Task<IActionResult> UploadImage(int eventoId)
+    {
+        try
+        {
+            var evento = await _eventoService.GetEventoByIdAsync(eventoId, true);
+            if (evento is null) return NoContent();
+
+            var file = Request.Form.Files[0];
+            if (file.Length > 0)
+            {
+                DeleteImage(evento.ImagemURL);
+                evento.ImagemURL = await SaveImage(file);
+            }
+            var eventoRetorno = await _eventoService.UpdateEvento(eventoId, evento);
+
+            return Ok(eventoRetorno);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao tentar incluir um evento. Erro: {ex.Message}");
+        }
+    }
+
+
     [HttpPost]
     public async Task<IActionResult> Post(EventoDTO model)
     {
@@ -113,5 +140,34 @@ public class EventosController : ControllerBase
         {
             return StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao tentar deletar um evento. Erro: {ex.Message}");
         }
+    }
+
+
+    [NonAction]
+    public async Task<string> SaveImage(IFormFile imageFile)
+    {
+        string imageName = new String(Path.GetFileNameWithoutExtension(imageFile.FileName)
+            .Take(10)
+            .ToArray()
+        ).Replace(' ', '-');
+
+        imageName = $"{imageName}{DateTime.UtcNow.ToString("yymmssfff")}{Path.GetExtension(imageFile.FileName)}";
+
+        var imagePath = Path.Combine(_environment.ContentRootPath, @"Resources/Images", imageName);
+
+        using (var fileStream = new FileStream(imagePath, FileMode.Create))
+        {
+            await imageFile.CopyToAsync(fileStream);
+        }
+
+        return imageName;
+    }
+
+    [NonAction]
+    public void DeleteImage(string imageName)
+    {
+        var imagePath = Path.Combine(_environment.ContentRootPath, @"Resources/Images", imageName);
+        if (System.IO.File.Exists(imagePath))
+            System.IO.File.Delete(imagePath);
     }
 }
