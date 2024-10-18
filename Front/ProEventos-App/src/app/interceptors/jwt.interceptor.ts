3addfe932cfa25613c6d92c64bcd977eae42d08c
Vinicius Silva <vsaguiar@outlook.com>
2024-10-18 13:07:14 -0300

    usuário delogado

--- Front/ProEventos-App/src/app/interceptors/jwt.interceptor.ts
+++ Front/ProEventos-App/src/app/interceptors/jwt.interceptor.ts
@@ -6,9 +6,9 @@ import {
   HttpEvent,
   HttpInterceptor
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { User } from '@app/models/Identity/user';
-import { take } from 'rxjs/operators';
+import { catchError, take } from 'rxjs/operators';
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
@@ -33,6 +33,13 @@ export class JwtInterceptor implements HttpInterceptor {
         }
       });
 
-    return next.handle(request);
+    return next.handle(request).pipe(
+      catchError(error => {
+        if (error) {
+          localStorage.removeItem('user');
+        }
+        return throwError(error);
+      })
+    );
   }
 }
